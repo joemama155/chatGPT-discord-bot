5f98f71c71d59a3832e74c38370ffd39bc4afb8e
Noah Huppert <3323504+Noah-Huppert@users.noreply.github.com>
2022-12-30 04:10:51 -0500

    made bot remember conversation history

--- src/bot.py
+++ src/bot.py
@@ -2,6 +2,10 @@ import discord
 from discord import app_commands
 from src import responses
 from src import log
+import time
+from typing import Optional
+import random
+import sys
 
 logger = log.setup_logger(__name__)
 
@@ -9,59 +13,89 @@ config = responses.get_config()
 
 isPrivate = False
 
+BACKUP_PROMPTS = []
+with open("backup-prompts.txt", 'r') as f:
+    for line in f:
+        BACKUP_PROMPTS.append(line.strip())
+
+message_history = {} # keys: non bot user ID, values: list of tuples where (is bot?, message)
+myname = "Bot"
+usernames = {} # keys: user IDs, values: strings
+
 
 class aclient(discord.Client):
     def __init__(self) -> None:
-        super().__init__(intents=discord.Intents.default())
+        intents = discord.Intents.default()
+        intents.messages = True
+        intents.message_content = True
+        super().__init__(intents=intents)
         self.tree = app_commands.CommandTree(self)
-        self.activity = discord.Activity(type=discord.ActivityType.watching, name="/chat | /help")
+        self.activity = discord.Activity(type=discord.ActivityType.watching)
+        
 
+async def gracefully_send_error(message: discord.Message):
+    response = await get_gpt_response(f"Argue with the following message, and end with a question: {message.content}")
 
-async def send_message(message, user_message):
-    await message.response.defer(ephemeral=isPrivate)
+    if response is None:
+        logger.info("Failed to get non-empty backup prompt response, so sending a question")
+        await respond(message, random.choice(BACKUP_PROMPTS))
+
+    await respond(message, response)
+
+async def get_gpt_response(prompt: str) -> Optional[str]:
+    logger.info("gtp prompt: %s", prompt)
+    response = await responses.handle_response(prompt)
+    tries = 0
+    while len(response) == 0 and tries < 5:
+        response = await responses.handle_response(prompt)
+        logger.info("gpt gave an empty response (tries: %d)", tries)
+        tries += 1
+        
+    if len(response) == 0:
+        logger.info("despite multiple attempts GPT responded with nothing")
+        return None
+
+    return response
+
+async def respond(received_message: discord.Message, response: str):
+    message_history[received_message.author.id].append((True, response))
+    await received_message.channel.send(response)
+
+def generate_history(user_id: int) -> str:
+    global myname
+    out = []
+
+    for history_item in message_history[user_id]:
+        name = usernames[user_id]
+        if history_item[0] is True:
+            name = myname
+
+        txt = history_item[1].replace('\n', '.')
+        out.append(f"{name}: {txt}")
+
+    return out
+
+async def send_message(message: discord.Message, user_message: str):
+    global myname
     try:
-        response = '> **' + user_message + '** - <@' + \
-            str(message.user.id) + '>\n\n'
-        response = f"{response}{user_message}{await responses.handle_response(user_message)}"
+        history = generate_history(message.author.id)
+
+        history.append(f"{myname}:")
+        response = await get_gpt_response("\n".join(history))
+        
+        if response is None:
+            logger.exception("tried to get a non empty response 5 times in a row but failed")
+            await gracefully_send_error(message)
+            return
+        
         if len(response) > 1900:
             # Split the response into smaller chunks of no more than 1900 characters each(Discord limit is 2000 per chunk)
-            if "```" in response:
-                # Split the response if the code block exists
-                parts = response.split("```")
-                # Send the first message
-                await message.followup.send(parts[0])
-                # Send the code block in a seperate message
-                code_block = parts[1].split("\n")
-                formatted_code_block = ""
-                for line in code_block:
-                    while len(line) > 1900:
-                        # Split the line at the 50th character
-                        formatted_code_block += line[:1900] + "\n"
-                        line = line[1900:]
-                    formatted_code_block += line + "\n"  # Add the line and seperate with new line
-
-                # Send the code block in a separate message
-                if (len(formatted_code_block) > 2000):
-                    code_block_chunks = [formatted_code_block[i:i+1900]
-                                         for i in range(0, len(formatted_code_block), 1900)]
-                    for chunk in code_block_chunks:
-                        await message.followup.send("```" + chunk + "```")
-                else:
-                    await message.followup.send("```" + formatted_code_block + "```")
-
-                # Send the remaining of the response in another message
-
-                if len(parts) >= 3:
-                    await message.followup.send(parts[2])
-            else:
-                response_chunks = [response[i:i+1900]
-                                   for i in range(0, len(response), 1900)]
-                for chunk in response_chunks:
-                    await message.followup.send(chunk)
+            await respond(message, response[:1900])
         else:
-            await message.followup.send(response)
+            await respond(message, response)
     except Exception as e:
-        await message.followup.send("> **Error: Something went wrong, please try again later!**")
+        await gracefully_send_error(message)
+        #await message.reply("> **Error: Something went wrong, please try again later!**")
         logger.exception(f"Error while sending message: {e}")
 
 
@@ -93,44 +127,45 @@ def run_discord_bot():
 
     @client.event
     async def on_ready():
+        global myname
+        myname = client.user.name
         await send_start_prompt(client)
         await client.tree.sync()
         logger.info(f'{client.user} is now running!')
 
-    @client.tree.command(name="chat", description="Have a chat with ChatGPT")
-    async def chat(interaction: discord.Interaction, *, message: str):
-        if interaction.user == client.user:
+    @client.event
+    async def on_message(message: discord.Message):
+        logger.info("got message '%s' from %s", message.content, message.author.name)
+
+        if message.author.id not in usernames:
+            usernames[message.author.id] = message.author.name
+
+        # Ignore messages from myself
+        if message.author == client.user:
+            logger.info("the message was from me, ignoring")
             return
-        username = str(interaction.user)
-        user_message = message
-        channel = str(interaction.channel)
+        
+        # Ignore messages not in the right channel
+        if str(message.channel.id) != str(config['discord_channel_id']):
+            logger.info("the message is not in the right channel (got: %s, needs: %s), ignoring", message.channel.id, config['discord_channel_id'])
+            return
+
+        # Record message history
+        if message.author.id not in message_history:
+            message_history[message.author.id] = []
+
+        message_history[message.author.id].append((False, message.content))
+
+        # Log the message
+        username = str(message.author)
+        user_message = message.content
+        channel = str(message.channel)
         logger.info(
             f"\x1b[31m{username}\x1b[0m : '{user_message}' ({channel})")
-        await send_message(interaction, user_message)
 
-    @client.tree.command(name="private", description="Toggle private access")
-    async def private(interaction: discord.Interaction):
-        global isPrivate
-        await interaction.response.defer(ephemeral=False)
-        if not isPrivate:
-            isPrivate = not isPrivate
-            logger.warning("\x1b[31mSwitch to private mode\x1b[0m")
-            await interaction.followup.send("> **Info: Next, the response will be sent via private message. If you want to switch back to public mode, use `/public`**")
-        else:
-            logger.info("You already on private mode!")
-            await interaction.followup.send("> **Warn: You already on private mode. If you want to switch to public mode, use `/public`**")
+        time.sleep(2)
 
-    @client.tree.command(name="public", description="Toggle public access")
-    async def public(interaction: discord.Interaction):
-        global isPrivate
-        await interaction.response.defer(ephemeral=False)
-        if isPrivate:
-            isPrivate = not isPrivate
-            await interaction.followup.send("> **Info: Next, the response will be sent to the channel directly. If you want to switch back to private mode, use `/private`**")
-            logger.warning("\x1b[31mSwitch to public mode\x1b[0m")
-        else:
-            await interaction.followup.send("> **Warn: You already on public mode. If you want to switch to private mode, use `/private`**")
-            logger.info("You already on public mode!")
+        await send_message(message, user_message)
 
     @client.tree.command(name="help", description="Show help for the bot")
     async def help(interaction: discord.Interaction):
