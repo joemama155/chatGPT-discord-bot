4e0b221900cf153cd99e710de6b9d46083e750a3
Noah Huppert <contact@noahh.io>
2022-12-30 19:33:14 -0500

    rewrote responses.py into openai_client.py file

---
+++ src/openai_client.py
@@ -0,0 +1,31 @@
+import openai
+from asgiref.sync import sync_to_async
+
+from typing import Optional
+
+class OpenAI:
+    """ API client for OpenAI.
+    """
+    async def create_completion(self, prompt: str) -> Optional[str]:
+        """ Given a prompt use OpenAI to complete the text.
+        Arguments:
+        - prompt: The text fed to the OpenAI model
+
+        Returns: The OpenAI completion or None if the model could not complete.
+        """
+        response = await sync_to_async(openai.Completion.create)(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=2048,
+            top_p=1,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
+        )
+
+        non_empty_responses = list(filter(lambda choice: len(choice.text) > 0, response.choices))
+        if len(non_empty_responses) == 0:
+            # Couldn't get any completions from OpenAI
+            return None
+
+        return non_empty_responses[0].text
